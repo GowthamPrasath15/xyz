0c22152af1f9f1883330e574b6ec965a105bb766
GowthamPrasath15 <GowthamPrasath15@users.noreply.github.com>
2021-01-13 20:48:52 +0530

    main app

--- AmmaCollege/src/main/java/com/ammacollege/details/AmmaCollegeApplication.java
+++ AmmaCollege/src/main/java/com/ammacollege/details/AmmaCollegeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmmaCollegeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to our app");
 		SpringApplication.run(AmmaCollegeApplication.class, args);
 	}
 
